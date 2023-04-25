79733e9ebdf3b21f8f48fb0d5d04a1b0a1fee826
wql12138 <113866353+wql12138@users.noreply.github.com>
2023-04-25 11:46:25 -0400

    Add files via upload

--- svm_train.m
+++ svm_train.m
@@ -1,13 +1,14 @@
 %ytr = b.Classlabel;
 %xtr = [time.kurtosis,time.mean,time.median,time.skewness,frequency.mean_freq,frequency.median_freq,frequency.peak_freq];
 
-clc;clear;
+clc;
+clear;
 x=[];y=[];
-for i=1:8
+for i=1:9
     mat_form = ['A0',num2str(i),'T.mat'];
     gdf_form = ['A0',num2str(i),'T.gdf'];
     load(mat_form);
-    xtmp = [all_features.time.kurtosis,all_features.time.mean,all_features.time.median,all_features.time.skewness,all_features.frequency.mean_freq,all_features.frequency.median_freq,all_features.frequency.peak_freq];
+    xtmp = [all_features.time.kurtosis,all_features.time.mean,all_features.time.median,all_features.time.skewness,all_features.time.var,all_features.time.zc,all_features.frequency.mean_freq,all_features.frequency.median_freq,all_features.frequency.peak_freq,reshape(all_features.frequency.bandpower,288,110)];
     x = [x;xtmp];
 
     [a,b] = sload(gdf_form);
@@ -19,7 +20,7 @@ xval = x(train_index+1:n,:);xtr = x(1:train_index,:);yval = y(train_index+1:n,:)
 
 T=1e5;
 k = 4;
-for pow = -15:1:10
+for pow = -5:1:7
 lambda = 2^pow;
 
 [W] = train_svm_mhinge_sgd(xtr, ytr, k, lambda, T);
